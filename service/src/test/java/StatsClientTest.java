5af451779e9e2c20c5bb29d3dd5a8de6399724f7
Kseniya <ksenia-belos@mail.ru>
2023-09-21 18:39:12 +0300

    fix: unused import

--- service/src/test/java/StatsClientTest.java
+++ service/src/test/java/StatsClientTest.java
@@ -1,6 +1,5 @@
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.explore.StatsClient;
 
 @SpringBootTest
 @RequiredArgsConstructor
